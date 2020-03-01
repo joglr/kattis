cf879e85a75d032af4735c7ca8915a02949326fd
Jonas Røssum <jonas.g.roessum@gmail.com>
2020-03-01 12:14:14 +0100

    Support running a single test

--- Test.java
+++ Test.java
@@ -8,46 +8,64 @@ public abstract class Test {
   abstract Testable instantiateTestable();
 
   public Test() throws IOException {
+    this(null);
+  }
+
+  public Test(Integer testID) throws IOException {
     Testable testable = instantiateTestable();
     // // Stopwatch total = new Stopwatch("total");
 
-    for (int i = 0; i < testable.getTestCount(); i++) {
-
-      FileReader inputFile = new FileReader(testable.getTestsFolder() + "/" + (i + 1) + ".in");
-      FileReader expectedOutputFile = new FileReader(testable.getTestsFolder() + "/" + (i + 1) + ".ans");
-
-      BufferedReader inputReader = new BufferedReader(inputFile);
-      BufferedReader expectedOutputReader = new BufferedReader(expectedOutputFile);
-
-      String result = testable.run(inputReader);
-      String[] lines = result.split("\n");
-
-      for (String line : lines) {
-
-        String expectedOutput = expectedOutputReader.readLine();
-
-        System.out.print(line);
-
-        if (line.equals(expectedOutput)) {
-          System.out.print(" ✔");
-        } else {
-          System.out.print(" × wrong output, expected: " + expectedOutput);
-        }
-
-        System.out.println();
+    if (testID == null) {
+      for (int i = 1; i <= testable.getTestCount(); i++) {
+        runTest(testable, i);
       }
+    } else runTest(testable, testID);
 
-      inputReader.close();
-      expectedOutputReader.close();
-    }
-
-    System.out.println();
     // total.printEllapsedTimeInMs("ran " + testable.getTestCount() + " tests from
     // folder " + testable.getTestsFolder());
+
+    System.out.println();
+  }
+
+  private void runTest(Testable testable, int i) throws IOException {
+    FileReader inputFile = new FileReader(
+      testable.getTestsFolder() + "/" + (i) + ".in"
+    );
+    FileReader expectedOutputFile = new FileReader(
+      testable.getTestsFolder() + "/" + (i) + ".ans"
+    );
+
+    BufferedReader inputReader = new BufferedReader(inputFile);
+    BufferedReader expectedOutputReader = new BufferedReader(
+      expectedOutputFile
+    );
+
+    String result = testable.run(inputReader);
+    String[] lines = result.split("\n");
+
+    for (String line : lines) {
+      String expectedOutput = expectedOutputReader.readLine();
+
+      System.out.print(line);
+
+      if (line.equals(expectedOutput)) {
+        System.out.print(" ✔");
+      } else {
+        System.out.print(" × wrong output, expected: " + expectedOutput);
+      }
+
+      System.out.println();
+    }
+
+    inputReader.close();
+    expectedOutputReader.close();
   }
 
   public static int[] getIntsFromLine(String input, String delimiter) {
-    return Arrays.stream(input.split(delimiter)).mapToInt(Integer::parseInt).toArray();
+    return Arrays
+      .stream(input.split(delimiter))
+      .mapToInt(Integer::parseInt)
+      .toArray();
   }
 
   public static int[] getIntsFromLine(String input) {
