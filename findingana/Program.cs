d0890d34168a1fbf28699ca0b8b577a8f8b4d790
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-09-12 11:36:57 +0200

    Solve findingana

---
+++ findingana/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace findingana
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string word = Console.ReadLine();
+      int indexOfA = word.IndexOf("a");
+      string ord = word.Substring(indexOfA, word.Length - indexOfA);
+      System.Console.WriteLine(ord);
+    }
+  }
+}
