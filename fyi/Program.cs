cb9d325a5aadd3fa304be1ec867425ce18beef1e
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-09-12 10:23:15 +0200

    Solve fyi

---
+++ fyi/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+// Main C# class
+
+class Program
+{
+  // Main C# method
+
+  static void Main(string[] args)
+  {
+    // Print "Hello World!" to the console
+
+    String input = Console.ReadLine().Trim();
+
+    if (input.StartsWith("555")) Console.WriteLine(1);
+    else Console.WriteLine(0);
+
+  }
+}
+
