a7ab62a7fb952fdc709fe6ce42a940e697ebf474
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-09-25 15:37:40 +0200

    Solve digitswap

---
+++ digitswap/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace digitswap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            System.Console.WriteLine(str[1] + "" + str[0]);
+        }
+    }
+}
