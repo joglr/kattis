141ef7413b1d6f61e4daf086746888955039aa60
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-09-25 18:04:17 +0200

    Solve autori

---
+++ autori/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace autori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            char[] chars = name.Split("-").Select(x => x.ToCharArray()[0]).ToArray();
+            string initials = new String(chars);
+            System.Console.WriteLine(initials);
+        }
+    }
+}
